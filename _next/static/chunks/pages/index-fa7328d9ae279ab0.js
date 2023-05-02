1b6efeebe42033ff1bf19810f87af93cc3aa0e4a
amcolash <amcolash@users.noreply.github.com>
2023-05-02 08:56:39 +0000

    deploy: 8da505f843588ed1664fdf89833315285922f8be

---
+++ _next/static/chunks/pages/index-fa7328d9ae279ab0.js
@@ -0,0 +1,2 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[405],{5728:function(n,u,t){(window.__NEXT_P=window.__NEXT_P||[]).push(["/",function(){return t(1878)}])},1878:function(n,u,t){"use strict";t.r(u),t.d(u,{default:function(){return _}});var r=t(5893);function _(){return(0,r.jsx)("div",{})}}},function(n){n.O(0,[774,888,179],(function(){return u=5728,n(n.s=u);var u}));var u=n.O();_N_E=u}]);
+//# sourceMappingURL=index-fa7328d9ae279ab0.js.map
